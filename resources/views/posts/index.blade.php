798860b1659e8ee74f0002a11476c41a49beb260
sosualfred <sosualfred@gmail.com>
2020-07-04 14:50:28 +0000

    fixed images not displaying on posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,14 +6,14 @@
          <div class="well">
              <div class="row">
                  <div class="col-md-4 col-sm-4">
-                     <img src="/storage/cover_images/{{$post->cover_image}}" alt="cover image"  style ='width: 100%' >
+                     <img src="{{ asset($post->cover_image) }}" alt="cover image"  style ='width: 100%' >
                  </div>
                  <div class="col-md-8 col-sm-8">
                     <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
                     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
                 </div>
              </div>
-            </div>  
+            </div>
        @endforeach
        {{$posts->links()}}
     @else
